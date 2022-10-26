192cc2a2c69313a2c4aacb1a0faf8c763fee4ac9
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-10-26 20:11:38 +0400

    Pushed the small challenges

---
+++ DAY10/day10.js
@@ -0,0 +1,21 @@
+const jonas = {
+    name: 'Jonas',
+    friends: ['Michel', 'Gerard', 'Mark']
+}
+
+console.log(`${jonas.name} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
+
+const jonasDetails = {
+    name:'Jonas',
+    birthyear:'2002',
+    job:'teacher',
+    hasDriverLicense:true,
+
+    calcAge : function(){
+        this.age = 2037 - 2002;
+        return this.age
+    }
+}
+
+
+console.log(`${jonasDetails.name} is a ${jonasDetails.calcAge()}-year old ${jonasDetails.job}, and ${jonasDetails.hasDriverLicense ? "he has a driver's license" : "he has no driver's license"}`)
