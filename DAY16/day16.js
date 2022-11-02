227f0de87c36204bf5430d5b3a7fb9a6110cb4e0
Scholaa <xolaineza@gmail.com>
2022-11-02 13:54:15 +0400

    Small notes for day 16

---
+++ DAY16/day16.js
@@ -0,0 +1,20 @@
+/**
+ * Query selector
+ * querySelector is a method of Document and it returns the first element that matches the specified selector, or group of selectors.
+ * If no matches are found, null is returned.
+ * Synthax:
+ * querySelector(selectors)
+ *
+ *
+ */
+
+/**
+ * addEventlistener
+ * The addEventListener method of the EventTarget interface sets up a function that will be called whenever the specified event is delivered to the target.
+ * Common targets are Element (or its children), Document, and Window.
+ * But the target may be any object that supports events
+ * Synthax:
+ * addEventListener(type, listener)
+ * addEventListener(type, listener, options)
+ * addEventListener(type, listener, useCapture)
+ */
