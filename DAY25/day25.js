a287c70725197a280ee54304d481e042969e2f15
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-11-10 15:09:48 +0400

    CHALLENGE FROM MY SIDE ADDED

--- DAY25/day25.js
+++ DAY25/day25.js
@@ -7,6 +7,7 @@
  * 
  */
 
+
 const gameEvents = new Map([
     [17, 'Goal'],
     [36, 'Substitution'],
@@ -18,12 +19,16 @@ const gameEvents = new Map([
     [80, 'Goal']
 ]);
 
-
 /**
  * 
  * 2. Remove the event from game events log
  * Yellow card from minute 64
  */
+
+gameEvents.delete(67);
+console.log(gameEvents)
+
+
 
 /**
  * 
@@ -31,8 +36,33 @@ const gameEvents = new Map([
  * A game has 90 minutes
  */
 
+let spansArray = [];
+let previous = 0;
+for(const [key] of gameEvents.entries()){
+    spansArray.push(key-previous);
+
+    previous = key
+
+    // console.log("SPAN", spansArray)
+
+}
+
+console.log("SPANS", spansArray)
+
+let sum = 0;
+for(const span of spansArray){
+    sum+=span;
+}
+
+console.log(`An event happened, on average, every ${sum/spansArray.length} minutes`)
+
+
 /**
  * Loop over the events and log them, marking whether it is in the first half or second
  * 
  * [FIRST HALF] 17: Goal
  */
+
+for(const [time, event] of gameEvents.entries()) 
+    console.log(`[${time<=45 ? 'FIRST' : 'SECOND'}HALF] ${time}: ${event}`);
+
