a8ea09a4c575bcc06f98a296c46cc6f849572e8e
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-11-14 15:54:57 +0400

    day28 content added

--- DAY28/day28.js
+++ DAY28/day28.js
@@ -72,3 +72,92 @@ const checkNumberOfVowels = (string)=>{
 }
 
 console.log(checkNumberOfVowels("Permanganate"))
+
+
+/**
+ * 5.Longest string in array
+ * 
+ */
+
+ console.log("5. ############################")
+
+ const findLongestString = (array)=>{
+    let longestString = '';
+    let longestLength = 0;
+    for(const string of array){
+        if(string.length > longestLength){
+            longestLength = string.length
+            longestString = string;
+        }
+    }
+
+    return longestString;
+ }
+
+ console.log(findLongestString(['mxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxe', 'meeevvvvvvvvvvvv', 'a', 'eeeeeee']))
+
+ /**
+ * 6. Most Commonly Used Character in String
+ * 
+ */
+
+  console.log("6. ############################")
+
+const findMostCommonlyUsed = (string)=>{
+    const temp = {}
+
+    for(const letter of string){
+        
+        if(Object.keys(temp).includes(letter)){
+            temp[letter] += 1
+
+        }
+        
+        else {
+            temp[letter] = 1;
+        }
+
+    }
+
+    let frequentLetter = '';
+    let frequency = 0;
+
+    for(const [key, el] of Object.entries(temp)){
+        if(el > frequency){
+            frequency = el;
+            frequentLetter = key
+        }
+    }
+
+    return frequentLetter;
+
+}
+
+console.log("MOST COMMON", findMostCommonlyUsed('Here we gggggggooo againo'))
+
+
+ /**
+ * 7. Two Strings Are Anagrams of Each Other
+ * 
+ * 
+ */
+
+  console.log("7. ############################")
+
+  const checkAnagrams = (firstString, secondString)=>{
+
+    let sameLetters = false;
+
+    if(firstString.length === secondString.length){
+        for(const letter of secondString.toLowerCase()){
+            if(firstString.toLowerCase().includes(letter)) sameLetters = true;
+            else return false
+        }
+
+    }
+   
+    return sameLetters;
+
+  }
+
+  console.log("SAME LETTERS ? ", checkAnagrams("Pacy", "YPCA"));
