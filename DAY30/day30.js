95e6bb824a794827d810a6dcf7c1acf582bd4405
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-11-16 21:14:03 +0400

    Pushed challenge

---
+++ DAY30/day30.js
@@ -0,0 +1,13 @@
+
+
+(function(){
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    
+    document.body.addEventListener('click', function(){
+        header.style.color = 'blue';
+    })
+
+})();
+
+
