0e3d91a6740f2e8fde16dc6b1a57601f8fe652e9
Scholaa <xolaineza@gmail.com>
2022-12-03 12:49:48 +0400

    Day 45

---
+++ DAY45/day45.js
@@ -0,0 +1,8 @@
+/**
+ * Transform property
+ * lets you rotate, scale, skew, or translate an element.
+ *
+ * translateX(x): Defines a translation, using only the value for the X-axis
+ *
+ * object.style.transform = translateX(x)
+ */
