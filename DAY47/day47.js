5e15dd1c7b13f63bff21b46ac8b0c1871847813b
Scholaa <xolaineza@gmail.com>
2022-12-04 14:58:57 +0400

    what you were supposed to do

--- DAY47/day47.js
+++ DAY47/day47.js
@@ -1,21 +1,26 @@
+const Car = function (make, speed) {
+	this.make = make;
+	this.speed = speed;
+	// this.accelerate = () => {
+	// 	this.speed += 10;
+	// 	console.log(this.speed);
+	// };
+	// this.brake = () => {
+	// 	this.speed -= 5;
+	// 	console.log(this.speed);
+	// };
+};
 
+Car.prototype.brake = function () {
+	this.speed -= 5;
+	console.log(this.speed);
+};
+Car.prototype.accelerate = function () {
+	this.speed += 10;
+	console.log(this.speed);
+};
 
-const Car = function(make, speed){
-    this.make = make;
-    this.speed = speed;
-    this.accelerate = ()=>{
-        this.speed+=10;
-        console.log(this.speed);
-    };
-    this.brake = ()=>{
-        this.speed-=5;
-        console.log(this.speed);
-    }
-
-}
-
-
-const small_car = new Car('BMW', 120);
+const small_car = new Car("BMW", 120);
 
 small_car.accelerate();
 small_car.brake();
