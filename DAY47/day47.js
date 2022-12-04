f2aa951e3f2e65be977081a64968b79b3c052ae8
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-12-04 14:33:54 +0400

    Added challenge

---
+++ DAY47/day47.js
@@ -0,0 +1,21 @@
+
+
+const Car = function(make, speed){
+    this.make = make;
+    this.speed = speed;
+    this.accelerate = ()=>{
+        this.speed+=10;
+        console.log(this.speed);
+    };
+    this.brake = ()=>{
+        this.speed-=5;
+        console.log(this.speed);
+    }
+
+}
+
+
+const small_car = new Car('BMW', 120);
+
+small_car.accelerate();
+small_car.brake();
