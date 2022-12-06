2e1833ea4247f3fa1a02fe991daa80f256816ee6
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2022-12-06 11:51:33 +0400

    Added day48

--- DAY48/day48.js
+++ DAY48/day48.js
@@ -1,7 +1,114 @@
 /**
+ * STATIC FUNCTIONS
+ * Allow us to call them without creating an instance of the class(prototype)
+ */
+
+
+class Person{
+	constructor(name = "PK", birthdate = 1500){
+		this.name = name;
+		this.birthdate = birthdate;
+	}
+
+	calcAge(){
+		return new Date().getFullYear - this.birthdate;
+	}
+
+	static hey(){
+		console.log(`Hey there. Person class here`)
+	}
+}
+
+Person.bye = ()=>{
+	console.log("SEE YOU SOON")
+}
+
+Person.hey();	//prints Hey there. Person class here
+Person.bye()	//prints SEE YOU SOON
+
+
+
+/**
+ * OBJECT.CREATE
+ * Creating a new object from a prototype
+ */
+
+
+ /**  
+ * @param {*} name 
+ */
+
+Person.prototype.updateName = function(name){
+	this.name = name
+}
+
+
+console.log("PRO", Person.prototype)				//prints PRO { updateName: [Function (anonymous)] }
+
+class Student{
+	constructor(name, school){
+		this.name = name;
+		this.school = school;
+	}
+}
+
+console.log("STUDENT ", Student.prototype)
+
+const NewStudent = Object.create(Person);
+
+console.log("STUDENT OBJ", NewStudent.prototype)	// prints STUDENT OBJ { updateName: [Function (anonymous)] }
+
+
+
+/**
+ * INTER CLASS INHERITANCE
+ * 
+ */
+
+const Human = function(name, birthdate){
+	this.name = name;
+	this.birthdate = birthdate;
+
+}
+
+Human.prototype.introduce = function(){
+	console.log(`Human ${this.name} born in ${this.birthdate}`)
+}
+
+const Teacher = function(name, birthdate, degree){	
+	Human.call(this, name, birthdate)
+	this.degree = degree
+
+}
+
+console.log(Teacher.prototype);							//Empty at this stage
+
+Teacher.prototype = Object.create(Human.prototype)		//bind the prototype of teacher to that of Human to be able to access Human methods
+
+console.log("TEACHER PROTOTYPE", Teacher.prototype);	//  Human {} 
+
+const allano = new Teacher("Allano", 1956, "SOME HCI")
+
+allano.introduce();
+
+console.log("ALLANO INSTANCE OF", allano instanceof Teacher && allano instanceof Human && allano instanceof Object) //true
+console.log("TEACHER CONST BEFORE", Teacher.prototype.constructor)
+
+Teacher.prototype.constructor = Teacher;
+
+console.log("TEACHER CONST AFTER", Teacher.prototype.constructor)
+console.log("ALLANO PROTO AFTER", allano.__proto__)
+
+
+
+
+
+/**
  * Challenge
  *
  */
+
+
 class Car {
 	constructor(make, speed) {
 		this.make = make;
