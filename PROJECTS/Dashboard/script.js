7f081888f12871d3ed9d91b5b6c2f6f4e6d3ebde
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2023-01-15 10:40:10 +0400

    Small dashboard changes

--- PROJECTS/Dashboard/script.js
+++ PROJECTS/Dashboard/script.js
@@ -1,6 +1,9 @@
 
-const stats = document.querySelector('.stats');
+/**
+ * Loading dashboard statistics data 
+ */
 
+const stats = document.querySelector('.stats');
 const statsDetails = [
     {
         title:"Total Patient",
@@ -56,3 +59,138 @@ statsDetails.forEach((stat)=>{
     stats.insertAdjacentHTML("beforeend", template)
 
 })
+
+
+/**
+ * Loading sample patient data for dashboard
+ */
+const patientsContainer = document.querySelector('.patients');
+
+const patientDetails = [
+    {
+        name:"Lady Mary",
+        profile:"./assets/profile.jpg",
+        age:50,
+        status:'Pending'
+    },
+    {
+        name:"Lady Mary",
+        profile:"./assets/profile.jpg",
+        age:50,
+        status:'On Recovery'
+    },{
+        name:"Lady Mary",
+        profile:"./assets/profile.jpg",
+        age:50,
+        status:'Rejected'
+    },{
+        name:"Lady Mary",
+        profile:"./assets/profile.jpg",
+        age:50,
+        status:'Recovered'
+    },
+]
+
+patientDetails.forEach((patient)=>{
+    const template = `    
+        <div class="pat-info">
+            <div class="pat-image">
+                <img
+                    src=${patient.profile}
+                    alt="patient's profile"
+                    class=""
+                />
+            </div>
+            <div class="pat">
+                <p>
+                    <span class="pat-name">${patient.name}</span><br /><span
+                        class="pat-age"
+                        >${patient.age} Years</span
+                    >
+                </p>
+
+                <p class="pat-status ${patient.status === "On Recovery" ? 'recovery' : patient.status.toLowerCase()}">${patient.status}</p>
+            </div>
+        </div>
+        <div class="divider"></div>
+        `
+    patientsContainer.insertAdjacentHTML("beforeend", template)
+    
+})
+
+
+/**
+ * Loading sample doctors data for dashboard
+ */
+const doctorsContainer = document.querySelector('.doctor-card');
+
+const doctorsDetails = [
+    {
+        name: 'Dr. Stephany',
+        image: "./assets/profile.jpg",
+        rating: 4.2,
+        title: 'Dentist',
+        address:`795 Folsom Ave, Suite 600<br />
+        San Francisco, CADGE 94107`
+    },
+    {
+        name: 'Dr. Lil Mbwa',
+        image: "./assets/profile.jpg",
+        rating: 4.2,
+        title: 'Gynecologist',
+        address:`795 Folsom Ave, Suite 600<br />
+        San Francisco, CADGE 94107`
+    },{
+        name: 'Dr Louis Ng',
+        image: "./assets/profile.jpg",
+        rating: 4.2,
+        title: 'Cardiologist',
+        address:`795 Folsom Ave, Suite 600<br />
+        San Francisco, CADGE 94107`
+    },
+    {
+        name: 'Dr. Stephany',
+        image: "./assets/profile.jpg",
+        rating: 4.2,
+        title: 'Dentist',
+        address:`795 Folsom Ave, Suite 600<br />
+        San Francisco, CADGE 94107`
+    },
+]
+
+doctorsDetails.forEach((doctor)=>{
+    const template = `    
+            <div>
+                <div class="doc-image">
+                    <img src=${doctor.image} alt="image" class="" />
+
+                    <div class="ratingBox">
+                        <i class="fa-solid fa-star fa-2xs"></i>
+                        <span>${doctor.rating}</span>
+                    </div>
+                </div>
+
+                <p class="doc-name">${doctor.name}</p>
+                <p class="doc-title">${doctor.title}</p>
+                <p class="doc-address">
+                   ${doctor.address}
+                </p>
+                <div class="container-icon">
+                    <div class="socials">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                    <div class="socials">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </div>
+                    <div class="socials">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                </div>
+            </div>
+        `
+    doctorsContainer.insertAdjacentHTML("beforeend", template)
+    
+})
+
+
+
