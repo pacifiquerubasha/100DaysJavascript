58b4dbe17319c0807af8ac019fa08054cf561b29
Pacifique Rubasha <p.kishinyambwe@alustudent.com>
2023-01-21 15:06:00 +0400

    Patient list filtering handled

--- PROJECTS/cyrrus/modules/patient.js
+++ PROJECTS/cyrrus/modules/patient.js
@@ -16,29 +16,54 @@ const patientFormSelects = document.querySelectorAll('.inputContainer-selectText
 
 handleToggleCustomSelectInputs(patientFormSelects);
 
+const loadPatientsList = (data)=>{
+    const patientListContainer = document.querySelector('.dataTable');
 
-
-const patientListContainer = document.querySelector('.dataTable');
-patients.forEach((patient, key)=>{
-    const template = `
-        <div class="data-table-row" id="patient-${key}">
-            <span><p>${patient.id}</p></span>
-            <span><img src=${patient.picture_url} class="profileImagePatient"/></span>                                                         
-            <span><p>${patient.firstname}</p></span>
-            <span><p>${patient.lastname}</p></span>
-            <span><p>${patient.gender}</p></span>
-            <span><p>${patient.email}</p></span>
-            <span><p>${patient.nationality}</p></span>
-            <span><p>${patient.blood_group}</p></span>
-            <span><p>${patient.marital_status}</p></span>
-            <span><p>${patient.card_no}</p></span>
-
-        </div>    
+    patientListContainer.textContent = '';
     
+    const headerTemplate = `
+    <div class="data-table-row table-header">
+        <span>ID</span>
+        <span>Profile</span>
+        <span>First Name</span>                                                         
+        <span>Last Name</span>
+        <span>Gender</span>
+        <span>Email</span>
+        <span>Nationality</span>
+        <span>Blood group</span>
+        <span>Marital status</span>
+        <span>Medical Card No</span>
+    </div>
     `
+    patientListContainer.insertAdjacentHTML('beforeend', headerTemplate);
 
-    patientListContainer.insertAdjacentHTML('beforeend', template);
-})
+
+
+    data.forEach((patient, key)=>{
+        const template = `
+            <div class="data-table-row" id="patient-${key}">
+                <span><p>${patient.id}</p></span>
+                <span><img src=${patient.picture_url} class="profileImagePatient"/></span>                                                         
+                <span><p>${patient.firstname}</p></span>
+                <span><p>${patient.lastname}</p></span>
+                <span><p>${patient.gender}</p></span>
+                <span><p>${patient.email}</p></span>
+                <span><p>${patient.nationality}</p></span>
+                <span><p>${patient.blood_group}</p></span>
+                <span><p>${patient.marital_status}</p></span>
+                <span><p>${patient.card_no}</p></span>
+    
+            </div>    
+        
+        `
+        patientListContainer.insertAdjacentHTML('beforeend', template);
+    })
+
+}
+
+loadPatientsList(patients);
+
+
 
 
 const tableRows = document.querySelectorAll('.data-table-row');
@@ -58,3 +83,14 @@ const navigateToDetailsPage = (data)=>{
         location.href = '../pages/unique-patient.html'
     
 }
+
+
+const filterInput = document.querySelector('.filter-patients');
+
+filterInput.addEventListener('keyup', (e)=>{
+
+    const filteredPatientList = patients.filter((patient)=> patient.firstname.toLocaleLowerCase().includes(e.target.value) || patient.lastname.toLocaleLowerCase().includes(e.target.value))
+    
+    loadPatientsList(filteredPatientList);
+
+})
